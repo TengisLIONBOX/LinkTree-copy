97339681d9ee986c4f67291ebab8def81af97195
Тэнгис Ариунболд <HP236500@G302-CC0145.local>
2024-01-17 16:22:40 +0800

    add links

--- src/data/linktree-data.ts
+++ src/data/linktree-data.ts
@@ -3,4 +3,12 @@ export const data = [
     name: "Hoolly project",
     link: "https://hoollymernapp.web.app/",
   },
+  {
+    name: "Boginoo url shortener",
+    link: "https://boginooshorter.vercel.app/login",
+  },
+  {
+    name: "News website",
+    link: "https://newsweb-five.vercel.app/",
+  },
 ];
